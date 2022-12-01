80832aae35983a7896a8eeabf685098fc9c2b6e8
harleyhoang123 <harley.hoang.work@gmail.com>
2022-12-02 01:32:32 +0700

    Change account info data

---
+++ src/main/java/vn/edu/fpt/notification/dto/cache/UserInfo.java
@@ -0,0 +1,32 @@
+package vn.edu.fpt.notification.dto.cache;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author : Hoang Lam
+ * @product : Charity Management System
+ * @project : Charity System
+ * @created : 21/11/2022 - 00:51
+ * @contact : 0834481768 - hoang.harley.work@gmail.com
+ **/
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@JsonPropertyOrder({"username", "email", "fullName", "avatar", "roles"})
+public class UserInfo implements Serializable {
+
+    private static final long serialVersionUID = -7300441418202647210L;
+    private String username;
+    private String email;
+    private String fullName;
+    private String avatar;
+    private List<String> roles;
+}
